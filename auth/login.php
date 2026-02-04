8976a4350992085668ce2fdbf271eb07087a3dab
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2026-02-04 00:21:34 -0600

    Contraseña

--- auth/login.php
+++ auth/login.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt->execute([$username]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($user && password_verify($password, $user['password'])) {
+    if ($user && $password === $user['password']) {
         $_SESSION['user'] = $user['usuario'];
         header('Location: ../public/index.php');
         exit;
